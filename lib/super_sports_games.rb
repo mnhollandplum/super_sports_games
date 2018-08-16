61faf7f2494d9c495a0b95735f1fa441b04f12f4
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-08-16 11:23:53 -0600

    cleans up some spaces

--- lib/super_sports_games.rb
+++ lib/super_sports_games.rb
@@ -7,29 +7,38 @@ puts "enter year of your games"
 year_response = gets.chomp
 games = Games.new(year_response)
 
+ages_response = []
+puts "Please enter an event for your games. i.e. soccer"
+name_response = gets.chomp.capitalize
+
+events = Event.new(name_response, ages_response)
+
+games.events << events
+
+puts "Please enter the ages of your players for #{events.name} i.e. 19, 22, 33. Add up to five ages for your players"
+ages_response << gets.chomp.to_i
+#put in a if statement that determines correct response for ages
+
 loop do
-  ages_response = []
+
   puts "do you have an event to add to your event y/n?"
   yes_no = gets.chomp
   if yes_no == "y"
 
-  puts "enter an event for your games. i.e. soccer"
+  ages_response = []
+  puts "Please enter another event for your games. i.e. tennis"
   name_response = gets.chomp.capitalize
-  puts "enter the ages of your players i.e. 19, 22, 33. Add up to five ages for your players"
-  ages_response << gets.chomp.to_i
-  #put in a if statement that determines correct response for ages
   events = Event.new(name_response, ages_response)
-
   games.events << events
 
-
+  puts "Please enter the ages of your players for #{events.name} i.e. 10, 11, 33. Add up to five ages for your players"
+  ages_response << gets.chomp.to_i
 
   elsif yes_no == "n"
     break
   else
     puts "please put y or n"
-
-
   end
 end
+
 puts games.summary
--- playground.rb
+++ playground.rb
@@ -10,13 +10,14 @@ games = Games.new(year_response)
 ages_response = []
 puts "Please enter an event for your games. i.e. soccer"
 name_response = gets.chomp.capitalize
+
 events = Event.new(name_response, ages_response)
+
 games.events << events
+
 puts "Please enter the ages of your players for #{events.name} i.e. 19, 22, 33. Add up to five ages for your players"
 ages_response << gets.chomp.to_i
 #put in a if statement that determines correct response for ages
-
-
 
 loop do
 
@@ -29,16 +30,15 @@ loop do
   name_response = gets.chomp.capitalize
   events = Event.new(name_response, ages_response)
   games.events << events
-  puts "Please enter the ages of your players for #{events.name} i.e. 10, 11, 33. Add up to five ages for your players"
 
+  puts "Please enter the ages of your players for #{events.name} i.e. 10, 11, 33. Add up to five ages for your players"
   ages_response << gets.chomp.to_i
 
   elsif yes_no == "n"
     break
   else
     puts "please put y or n"
-
-
   end
 end
+
 puts games.summary
